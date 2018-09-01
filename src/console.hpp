461d2f445c661ecff61ffad93a574b4560c6ba4f
Hung Dang <hungptit@gmail.com>
2018-09-01 00:20:02 -0400

    Working on a better navigation algorithm.

--- src/console.hpp
+++ src/console.hpp
@@ -4,17 +4,26 @@
 #include <vector>
 
 namespace console {
-    constexpr int BACKSPACE = 127;
-    constexpr int CTRL_D = 4;
-    constexpr int ENTER = 10;
-    constexpr int ESC = 27;
-    constexpr int UP = 661;
-    constexpr int DOWN = 651;
+    enum Key : int {
+        BACKSPACE = 127,
+        DOWN = 258,
+        UP = 259,
+        LEFT = 269,
+        RIGHT = 261,
+        ESC = 27,
+        ENTER = 10, // Mark the current item.
+        CTRL_D = 4,
+        CTRL_C = 3,
+        CTRL_N = 14,
+        CTRL_O = 15,
+        CTRL_P = 16,
+        CTRL_S = 19,
+    };
 
     template <typename Policy> class Screen : public Policy {
       public:
-        template<typename Params>
-        Screen(Params && params) : Policy(std::forward<Params>(params)) {
+        template <typename Params>
+        Screen(Params &&params) : Policy(std::forward<Params>(params)) {
             init();
         }
 
@@ -22,40 +31,70 @@ namespace console {
 
         void run() {
             bool isok = true;
+            const int nrows = LINES - 3;
             while (isok) {
                 int key = getch();
-                switch (key) {
-                case console::BACKSPACE:
-                    if (!Policy::pattern.empty()) {
-                        Policy::pattern.pop_back();
-                    }
-                    break;
-                case ENTER:
-                    break;
-                case ESC:
-                    isok = false;
-                    break;
-                case UP:
-                    break;
-                case DOWN:
-                    break;
-                case CTRL_D:
-                    isok = false;
-                    break;
-                default:
+
+                if (std::isalpha(key) || std::isdigit(key) || isvalid(key)) {
                     Policy::pattern.push_back(key);
+                    update();
+                    continue;
                 }
 
-                // Update data
-                Policy::update_data();
+                mvprintw(0, 0, "key: %d", key);
 
-                // Update view
-                print_input();
-                print_output();
+                if (key == Key::UP) {
+                    if (current_position >  0) {
+                        if ((nrows - current_position + 1) < Policy::data.size()) {
+                            --current_position;
+                        }
+                    } else {
+                        // Update view.
+                    }
+                    print_output();
+                    continue;
+                }
+
+                if (key == Key::DOWN) {
+                    if (current_position < nrows) {
+                        ++current_position;
+                    } else {
+                        // Update view 
+                    }
+                    print_output();
+                    continue;
+                }
+
+                if ((key == Key::ESC) || (key == Key::CTRL_D) || (key == Key::CTRL_C)) {
+                    isok = false;
+                    continue;
+                }
+
+                if (key == Key::BACKSPACE) {
+                    if (Policy::pattern.empty()) continue;
+                    Policy::pattern.pop_back();
+                    update();
+                }
+
+                if (key == Key::ENTER) {
+                    mvprintw(1, 0, "current_position: %d", current_position);
+                }
             }
         }
 
       private:
+        bool isvalid(int ch) {
+            return (ch == ':') || (ch == '(') || (ch == ')') || (ch == '.') || (ch == '?') ||
+                   (ch == ']') || (ch == '[') || (ch == '_') || (ch == '-') || (ch == '*');
+        }
+
+        void update() {
+            Policy::update_data();
+            print_input();
+            print_output();
+            refresh();
+        }
+
         void print_output() {
             const int nrows = LINES - 3;
             const int ncols = COLS;
@@ -63,24 +102,24 @@ namespace console {
             int idx = nrows;
 
             // Print out content.
+            mvprintw(0, 0, "-> current_position: %d", current_position);
             for (auto const &aline : Policy::data) {
-                if (idx == nrows)
+                if (idx == current_position)
                     mvprintw(idx, 0, "> %s", create_line(aline, ncols - 10).data());
                 else
                     mvprintw(idx, 0, "  %s", create_line(aline, ncols - 10).data());
-                    
+
                 if (--idx < 0) break;
             }
 
             std::string empty_line(ncols, ' ');
-            while (idx > -1) {
+            while (idx > 3) {
                 mvprintw(idx, 0, empty_line.data());
                 --idx;
             }
 
             // Make sure that the cursor is at the input.
             move(nrows + 2, Policy::pattern.size() + 2);
-            current_position = nrows;
         }
 
         void print_input() {
@@ -106,6 +145,8 @@ namespace console {
             initscr();
             refresh();
             noecho();
+            keypad(stdscr, TRUE);
+            raw();
 
             // Init view
             Policy::init_data();
@@ -118,7 +159,9 @@ namespace console {
         }
 
         // The current position of a line cursor.
-        int current_position = 0;
+        int current_position;
+        int begin_view;
+        int end_view;
     };
 
 } // namespace console
